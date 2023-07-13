9994706435ef93048a87e1c42cb6a3a426d23bb7
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2023-07-13 14:11:23 +0600

    Настроено включение в логи значение параметров

--- BookShop/Startup.cs
+++ BookShop/Startup.cs
@@ -1,5 +1,7 @@
+using BookShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,13 +21,16 @@ namespace BookShop
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.EnableSensitiveDataLogging(true);
+                options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]);
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
