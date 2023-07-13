7e8a658ff95a3ac0c7fe69037517a33c47a4e887
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2023-07-13 14:09:13 +0600

    Создана модель Book

---
+++ BookShop/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookShop.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public DateTime PublishedOn{ get; set; }
+    }
+}
---
+++ BookShop/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookShop.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
