27420189edf68acd522967e692beb282d300a07b
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2023-07-14 09:52:55 +0600

    Добавлены поля категории и специальной цены

--- BookShop/Migrations/20230714030737_Book_RetailPrice_Category.Designer.cs
+++ BookShop/Migrations/20230714030737_Book_RetailPrice_Category.Designer.cs
@@ -4,14 +4,16 @@ using BookShop.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookShop.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230714030737_Book_RetailPrice_Category")]
+    partial class Book_RetailPrice_Category
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BookShop/Migrations/20230714030737_Book_RetailPrice_Category.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookShop.Migrations
+{
+    public partial class Book_RetailPrice_Category : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "RetailPrice",
+                table: "Books",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "RetailPrice",
+                table: "Books");
+        }
+    }
+}
