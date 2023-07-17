7fad128576b272a31e0dcbd8eeb500b0df050655
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2023-07-17 10:28:23 +0600

    Добавление базового интерфейса для паттерна Repository

---
+++ BookShop/Repo/IRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace BookShop.Repo
+{
+    public interface IRepositoryBase<T>
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
